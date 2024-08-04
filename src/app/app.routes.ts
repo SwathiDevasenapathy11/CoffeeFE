b74b2ec27a3329de95eea0e732f5fd9bd4e1288f
Devaswathi <swathidevasenapathy@gmail.com>
2024-08-04 14:12:15 +0530

    Created folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"shared",loadChildren:()=>import ('./shared/shared-routing.module').then(m=>m.SharedRoutingModule)
+    },
+    {
+        path:"user",loadChildren:()=>import ('./user/user-routing.module').then(m=>m.UserRoutingModule)
+    },
+];
